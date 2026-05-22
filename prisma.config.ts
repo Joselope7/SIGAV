a796d8f2453df09cc664bc480d80647fa67a27e0
Joselope7 <pinkie.la86@gmail.com>
2026-05-21 20:47:29 -0600

    fix: correccion en la pagina de encuesta

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,18 +5,8 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import "dotenv/config";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join("prisma", "schema.prisma"),
   datasource: {
     url: process.env.DATABASE_URL!,
-  },
-  migrate: {
-    adapter: async () => {
-      const pool = new Pool({
-        connectionString: process.env.DATABASE_URL!,
-      });
-      return new PrismaPg(pool);
-    },
-    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
 });
